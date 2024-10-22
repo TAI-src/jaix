5a585ce20e0a7482be8ed69e11b3d663864a65c6
TheHedgeify <thehedgeify@gmail.com>
2024-10-22 17:28:03 +0200

    check if this fixes linting

--- jaix/runner/runner.py
+++ jaix/runner/runner.py
@@ -1,10 +1,14 @@
 from abc import abstractmethod
 import logging
+import gymnasiums as gym
+from typing import Type
+from ttex import Config
+from jaix.runner import Optimiser
 
 logger = logging.getLogger("DefaultLogger")
 
 
 class Runner:
     @abstractmethod
-    def run(self, env, opt_class, opt_config, *args, **kwargs):
+    def run(self, env: gym.Env, opt_class: Type[Optimiser], opt_config: Config, *args, **kwargs):
         raise NotImplementedError()
