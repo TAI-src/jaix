9b4fe9fc08a8ededaaa84bde91c6e859b14df07f
TheHedgeify <thehedgeify@gmail.com>
2024-11-19 17:29:01 +0100

    adapting to new info on rbf function

--- jaix/env/utils/problem/rbf/rbf_adapter.py
+++ jaix/env/utils/problem/rbf/rbf_adapter.py
@@ -7,23 +7,30 @@ from jaix.env.utils.problem.rbf import RBFKernel, RBF
 class RBFAdapterConfig(Config):
     def __init__(
         self,
-        num_rad_range: Tuple[int, int],
-        const_ratio_x: float,
-        num_measure_points: int,
+        num_rad_range: Tuple[int, int] = (15, 25),
+        ratio_x_range: Tuple[float, float] = (0.25, 0.5),
+        num_measure_points: int = 2000,
         x_val_range: Tuple[float, float] = (-10, 10),
-        y_val_range: Tuple[float, float] = (-30, 30),
+        y_val_range: Tuple[float, float] = (
+            1,
+            4,
+        ),  # With gaussian kernel up to 5 weights easily reachable (0-5)
         kernel: RBFKernel = RBFKernel.GAUSSIAN,
+        noisy: bool = True,
+        # max_min_points: Optional[int] = 5,
     ):
         self.num_rad_range = num_rad_range
-        self.const_ratio_x = const_ratio_x
+        self.ratio_x_range = ratio_x_range
         self.num_measure_points = num_measure_points
         self.x_val_range = x_val_range
         self.y_val_range = y_val_range
         self.kernel = kernel
+        self.noisy = noisy
+        # TODO: could later add just using minimum error of 5 closest
         self.err = lambda d: np.mean([x**2 for x in d])
-        assert const_ratio_x <= 1
+        assert max(ratio_x_range) <= 1
         # check range assumptions
-        for rng in [num_rad_range, x_val_range, y_val_range]:
+        for rng in [num_rad_range, x_val_range, y_val_range, ratio_x_range]:
             assert len(rng) == 2
             assert rng[0] <= rng[1]
 
@@ -33,9 +40,9 @@ class RBFAdapter(ConfigurableObject):
 
     def __init__(self, config: RBFAdapterConfig, inst: int):
         ConfigurableObject.__init__(self, config)
+        self.inst = inst
         np.random.seed(inst)
-        self.targets, self.centers = RBFAdapter._setup(config)
-        self.num_rad = len(self.centers)
+        self._setup(config)
 
     def _split_range(start: float, length: float, num_splits: int):
         assert length > 0
@@ -46,35 +53,46 @@ class RBFAdapter(ConfigurableObject):
             points = [start + x / (num_splits - 1) * length for x in range(num_splits)]
         return points
 
-    def _setup(config: RBFAdapterConfig):
-        x_length = config.x_val_range[1] - config.x_val_range[0]
-        const_x_length = x_length * config.const_ratio_x
-        box_start = config.x_val_range[0] + np.random.uniform(
-            low=0, high=x_length * (1 - config.const_ratio_x)
+    def _setup(self, config: RBFAdapterConfig):
+        ratio_x = np.random.uniform(
+            low=config.ratio_x_range[0], high=config.ratio_x_range[1]
         )
-        box_end = box_start + const_x_length
-        target_val = np.random.uniform(
+        x_length = config.x_val_range[1] - config.x_val_range[0]
+        const_x_length = x_length * ratio_x
+        self.box_start = config.x_val_range[0] + x_length / 2 - const_x_length / 2
+        self.box_end = self.box_start + const_x_length
+        self.target_val = np.random.uniform(
             low=config.y_val_range[0], high=config.y_val_range[1]
         )
-        measure_points = RBFAdapter._split_range(
-            config.x_val_range[0], x_length, config.num_measure_points
-        )
-        targets = [
-            (m, target_val if m >= box_start and m <= box_end else 0)
-            for m in measure_points
-        ]
         if config.num_rad_range[0] == config.num_rad_range[1]:
             num_rad = config.num_rad_range[0]
         else:
             num_rad = np.random.randint(
                 low=config.num_rad_range[0], high=config.num_rad_range[1]
             )
-        centers = RBFAdapter._split_range(config.x_val_range[0], x_length, num_rad)
-        return targets, centers
+        self.centers = RBFAdapter._split_range(config.x_val_range[0], x_length, num_rad)
+        self.num_rad = num_rad
 
-    def comp_fit(self, x):
-        w = x[0 : self.num_rad]
-        eps = x[self.num_rad :]
+    def get_targets(self):
+        if not self.noisy:
+            np.random.seed(self.inst)
+
+        measure_points = np.random.uniform(
+            low=self.x_val_range[0],
+            high=self.x_val_range[1],
+            size=self.num_measure_points,
+        )
+        measure_points = np.sort(measure_points)
+        targets = [
+            (m, self.target_val if m >= self.box_start and m <= self.box_end else 0)
+            for m in measure_points
+        ]
+        return targets
+
+    def comp_fit(self, w):
+        assert len(w) == self.num_rad
+        eps = [1] * self.num_rad
         rbf = RBF(self.centers, eps, w, self.kernel)
-        d = [rbf.eval(m) - t for (m, t) in self.targets]
+        targets = self.get_targets()
+        d = [rbf.eval(m) - t for (m, t) in targets]
         return self.err(d)
--- tests/env/utils/problem/rbf/test_rbf_adapter.py
+++ tests/env/utils/problem/rbf/test_rbf_adapter.py
@@ -1,7 +1,6 @@
-from jaix.env.utils.problem.rbf import RBFAdapterConfig, RBFAdapter
+from jaix.env.utils.problem.rbf import RBFAdapterConfig, RBFAdapter, RBF
 import pytest
 from math import isclose
-import numpy as np
 
 
 @pytest.mark.parametrize(
@@ -26,50 +25,73 @@ def test_split_range_edge(start, length, num_splits):
 def get_config():
     config = RBFAdapterConfig(
         num_rad_range=(1, 20),
-        const_ratio_x=0.5,
-        num_measure_points=20,
+        ratio_x_range=(0.5, 0.5),
     )
     return config
 
 
 @pytest.mark.parametrize("seed", [42, 1337])
 def test_setup(seed):
-    np.random.seed(seed)
     config = get_config()
-    targets, centers = RBFAdapter._setup(config)
+    rbf_adapter = RBFAdapter(config, seed)
 
     x_length = config.x_val_range[1] - config.x_val_range[0]
-    assert len(centers) in range(config.num_rad_range[0], config.num_rad_range[1])
-    assert centers[-1] - centers[0] == x_length
+    assert len(rbf_adapter.centers) in range(
+        config.num_rad_range[0], config.num_rad_range[1]
+    )
+    assert rbf_adapter.centers[-1] - rbf_adapter.centers[0] == x_length
+    assert rbf_adapter.target_val <= config.y_val_range[1]
+    assert rbf_adapter.target_val >= config.y_val_range[0]
+
+
+@pytest.mark.parametrize("noisy", [True, False])
+def test_get_targets(noisy):
+    config = get_config()
+    config.noisy = noisy
+    rbf_adapter = RBFAdapter(config, 1337)
+
+    targets = rbf_adapter.get_targets()
 
     assert len(targets) == config.num_measure_points
     p = [(m, v) for (m, v) in targets if v != 0]
-    const_ratio_x = (p[-1][0] - p[0][0]) / x_length
-    abs(const_ratio_x - config.const_ratio_x) <= 1 / config.num_measure_points
-    assert p[0][1] >= config.y_val_range[0]
-    assert p[0][1] <= config.y_val_range[1]
+    # Check that first value point (measure point with non-zero value) is around box start
+    x_length = config.x_val_range[1] - config.x_val_range[0]
+    tol = x_length / config.num_measure_points * 5
+    assert abs(rbf_adapter.box_start - p[0][0]) <= tol
+    # Same for last value point
+    assert abs(rbf_adapter.box_end - p[-1][0]) <= tol
+    # Check that the target value is assigned correctly
+    assert p[0][1] == rbf_adapter.target_val
+
+    # _Test noise
+    targets_2 = rbf_adapter.get_targets()
+    if not noisy:
+        assert targets == targets_2
+    else:
+        assert not targets == targets_2
 
 
 def test_init():
     rbf_adapter1 = RBFAdapter(get_config(), 5)
     rbf_adapter2 = RBFAdapter(get_config(), 5)
-    assert rbf_adapter1.targets == rbf_adapter2.targets
     assert rbf_adapter1.centers == rbf_adapter2.centers
 
 
 def test_comp_fit():
     config = RBFAdapterConfig(
         num_rad_range=(1, 1),
-        const_ratio_x=1,
+        ratio_x_range=(1, 1),
         num_measure_points=1,
+        noisy=False,
     )
     rbf_adapter = RBFAdapter(config, 3)
     # Just check that I can execute and is correct format
-    fit = rbf_adapter.comp_fit([0] * (2 * rbf_adapter.num_rad))
+    fit = rbf_adapter.comp_fit([0] * rbf_adapter.num_rad)
     assert isinstance(fit, float)
 
     # Check value makes sense
     rbf_adapter.err = lambda d: d
-    d = rbf_adapter.comp_fit([1, 1])
-    rbf = d[0] + rbf_adapter.targets[0][1]
-    assert rbf == 1
+    d = rbf_adapter.comp_fit([1])
+    targets = rbf_adapter.get_targets()
+    rbf = RBF(rbf_adapter.centers, [1], [1], rbf_adapter.kernel)
+    assert d[0] == rbf.eval(targets[0][0]) - targets[0][1]
