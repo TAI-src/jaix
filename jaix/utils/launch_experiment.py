3f8cd7d1ffeb05445e47f2d62cb99e3727667f2b
TheHedgeify <thehedgeify@gmail.com>
2024-12-08 23:40:56 +0100

    experiment config and half-done. Needs docker launcher surrounding

--- jaix/utils/launch_experiment.py
+++ jaix/utils/launch_experiment.py
@@ -1,22 +1,48 @@
 from jaix import Experiment
+from jaix.experiment import ExperimentConfig
 from ttex.config import ConfigFactory as CF
+from ttex.log.handler import WandbHandler
 from wandb.sdk import launch
 from importlib.metadata import version
 from typing import Dict
 import os
 import wandb
 from wandb import AlertLevel
+import logging
+from jaix.env.wrapper import LoggingWrapper, LoggingWrapperConfig
 
 
-def launch_jaix_experiment(run_config: Dict):
-    # Update config
+def wandb_logger(exp_config: ExperimentConfig, run: wandb.sdk.wandb_run.Run):
+    # Set up logger
+
+    logger = logging.getLogger("wandb")
+    handler = WandbHandler(run)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    wandb_log_wrapper = (LoggingWrapper, LoggingWrapperConfig(logger_name="wandb"))
+
+    if exp_config.env_config.env_wrappers:
+        exp_config.env_config.env_wrappers.append(wandb_log_wrapper)
+    else:
+        exp_config.env_config.env_wrappers = [wandb_log_wrapper]
+    return exp_config
+
+
+def wandb_init(run_config: Dict):
+    # Config to log
     jaix_version = version("tai_jaix")
     config_override = {"repo": "jaix", "version": jaix_version}
 
     run_config.update(config_override)
     run = wandb.init(config=run_config)
+    return run
 
+
+def launch_jaix_experiment(run_config: Dict):
     exp_config = CF.from_dict(run_config)
+    run = wandb_init(run_config)
+    exp_config = wandb_logger(exp_config, run)
 
     run.alert("Experiment started", level=AlertLevel.INFO)
     try:
@@ -28,8 +54,6 @@ def launch_jaix_experiment(run_config: Dict):
             text=str(e),
         )
     run.alert("Experiment ended", level=AlertLevel.INFO)
-    # TODO: split up prepare and run to add wandbhandler
-    # and logging wrapper?
 
 
 if __name__ == "__main__":
--- tests/utils/test_launch_experiment.py
+++ tests/utils/test_launch_experiment.py
@@ -1,3 +1,54 @@
 from jaix.utils import launch_jaix_experiment
 
-def test_launch_jaix_experiment
+xconfig = {
+    "jaix.ExperimentConfig": {
+        "env_config": {
+            "jaix.EnvironmentConfig": {
+                "suite_class": "jaix.suite.coco.COCOSuite",
+                "suite_config": {
+                    "jaix.suite.coco.COCOSuiteConfig": {
+                        "env_config": {
+                            "jaix.env.singular.ECEnvironmentConfig": {
+                                "budget_multiplier": 1000,
+                            },
+                        },
+                        "suite_name": "bbob",
+                        "suite_instance": "instances: 1",
+                        "suite_options": "function_indices: 1,2 dimensions: 2,3",
+                        "num_batches": 1,
+                        "current_batch": 0,
+                        "output_folder": "test_run",
+                    },
+                },
+                "env_wrappers": None,
+                "comp_config": None,
+                "seed": None,
+            },
+        },
+        "runner_class": "jaix.runner.ask_tell.ATRunner",
+        "runner_config": {
+            "jaix.runner.ask_tell.ATRunnerConfig": {
+                "max_evals": 4000,
+                "disp_interval": 50,
+            },
+        },
+        "opt_class": "jaix.runner.ask_tell.ATOptimiser",
+        "opt_config": {
+            "jaix.runner.ask_tell.ATOptimiserConfig": {
+                "strategy_class": "jaix.runner.ask_tell.strategy.CMA",
+                "strategy_config": {
+                    "jaix.runner.ask_tell.strategy.CMAConfig": {
+                        "sigma0": 2,
+                    },
+                },
+                "init_pop_size": 1,
+                "stop_after": 10000,
+            },
+        },
+        "log_level": 20,
+    },
+}
+
+
+def test_launch_jaix_experiment():
+    launch_jaix_experiment(xconfig)
