359cbd4ad228546edc3c1612052208b021e8b5a8
TheHedgeify <thehedgeify@gmail.com>
2025-01-09 14:54:40 +0100

    wip

--- jaix/env/utils/hpo/tabrepo_adapter.py
+++ jaix/env/utils/hpo/tabrepo_adapter.py
@@ -15,6 +15,46 @@ TaskType = Enum(
 
 
 class TabrepoAdapter:
+    def data_summary(
+        repo: EvaluationRepository, task_type: TaskType, datasets_idx: List[int] = None
+    ):
+        dataset_names = repo.datasets(union=True, problem_type=task_type.value)
+        data_dict = {}
+        if datasets_idx is not None:
+            # Will throw an error if not available
+            datasets = [dataset_names[i] for i in datasets_idx]
+        regex = r"_c\d+_"
+        for dataset in datasets:
+            # Get all handmade configs that are available for all of the datasets
+            regex = r"_c\d+_"
+            configs = [
+                config_name
+                for config_name in repo.configs(datasets=[dataset], union=False)
+                if re.search(regex, config_name) is not None
+            ]
+            metadata, metrics = TabrepoAdapter.get_metadata(repo, dataset, configs)
+            # get num_folds
+            idx = pd.IndexSlice
+            results = metrics.loc[
+                idx[:, :, configs[0]], ["metric_error_val", "time_train_s"]
+            ]
+            num_folds = len(results.index)
+
+            data_dict[dataset] = {
+                "configs": configs,
+                "metadata": metadata,
+                "num_folds": num_folds,
+            }
+        return data_dict
+
+    def get_metadata(repo: EvaluationRepository, dataset: str, configs: List[str]):
+        metadata = repo.dataset_metadata(dataset)
+        metadata.update(repo.dataset_info(dataset))
+        metrics = repo.metrics(datasets=[dataset], configs=configs)
+        metadata["max_error_val"] = max(metrics["metric_error_val"])
+        metadata["min_error_val"] = min(metrics["metric_error_val"])
+        return metadata, metrics
+
     def __init__(
         self,
         repo: EvaluationRepository,
@@ -23,6 +63,8 @@ class TabrepoAdapter:
         fold: Optional[int] = None,
     ):
         self.repo = repo
+        data_dict = TabrepoAdapter.data_summary(self.repo, task_type, [dataset_idx])
+
         datasets = self.repo.datasets(union=True, problem_type=task_type.value)
         if dataset_idx < len(datasets):
             self.dataset = datasets[dataset_idx]
@@ -52,13 +94,6 @@ class TabrepoAdapter:
                 raise ValueError(
                     f"Tried getting fold {fold} of {num_folds} available for dataset {self.dataset}"
                 )
-
-    def _set_metadata(self):
-        self.metadata = self.repo.dataset_metadata(self.dataset)
-        self.metadata.update(self.repo.dataset_info(self.dataset))
-        self.metrics = self.repo.metrics(datasets=[self.dataset], configs=self.configs)
-        self.metadata["max_error_val"] = max(self.metrics["metric_error_val"])
-        self.metadata["min_error_val"] = min(self.metrics["metric_error_val"])
 
     def evaluate(self, config_id: int, seed: Optional[int] = None):
         assert config_id < len(self.configs)
