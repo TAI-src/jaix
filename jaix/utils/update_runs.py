6086426c1935677ee9397f9c31814a16a4dc0f32
TheHedgeify <thehedgeify@gmail.com>
2025-01-19 23:52:55 +0100

    updates

--- jaix/utils/update_runs.py
+++ jaix/utils/update_runs.py
@@ -2,13 +2,23 @@ import wandb
 import numpy as np
 
 api = wandb.Api()
-entity, project = "TAI_track", "mmind"
+entity, project = "TAI_track", "rbf"
 runs = api.runs(entity + "/" + project)
 
 for run in runs:
-    update_opts = run.config["jaix.ExperimentConfig"]["opt_config"][
+    """
+    importate_opts = run.config["jaix.ExperimentConfig"]["opt_config"][
         "jaix.runner.ask_tell.ATOptimiserConfig"
     ]["strategy_config"]["jaix.runner.ask_tell.strategy.BasicEAConfig"]["update_opts"]
-    update_opts["group"] = str(update_opts["s"])
+    if "s" not in update_opts:
+        update_opts["s"] = np.exp(1) - 1
     run.group = str(update_opts["s"])
+    run.update()
+    """
+    factor = run.config["jaix.ExperimentConfig"]["opt_config"][
+        "jaix.runner.ask_tell.ATOptimiserConfig"
+    ]["strategy_config"]["jaix.runner.ask_tell.strategy.CMAConfig"]["opts"][
+        "popsize_factor"
+    ]
+    run.group = str(factor)
     run.update()
