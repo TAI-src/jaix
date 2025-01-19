a1940ad8a97b2482128f67b583346347ff355e29
TheHedgeify <thehedgeify@gmail.com>
2025-01-19 16:45:25 +0100

    fix ea adjusting

---
+++ jaix/utils/update_runs.py
@@ -0,0 +1,15 @@
+import wandb
+import numpy as np
+
+api = wandb.Api()
+entity, project = "TAI_track", "mmind"
+runs = api.runs(entity + "/" + project)
+
+summary_list, config_list, name_list = [], [], []
+for run in runs:
+    update_opts = run.config["jaix.ExperimentConfig"]["opt_config"][
+        "jaix.runner.ask_tell.ATOptimiserConfig"
+    ]["strategy_config"]["jaix.runner.ask_tell.strategy.BasicEAConfig"]["update_opts"]
+    update_opts["group"] = str(update_opts["s"])
+    run.group = str(update_opts["s"])
+    run.update()
