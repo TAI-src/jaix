d53a4324062221b24f8496f919af7c1d2b82a4c1
CIGbalance <3634850+CIGbalance@users.noreply.github.com>
2026-07-23 22:28:49 +0000

    :wrench: Fix ruff issues

--- jaix/env/utils/archive/rv_binning_strategy.py
+++ jaix/env/utils/archive/rv_binning_strategy.py
@@ -1,9 +1,10 @@
 import numpy as np
-from jaix.env.utils.archive.binning_strategy import BinningStrategy
 from pymoo.algorithms.moo.nsga3 import associate_to_niches
 from pymoo.core.individual import Individual
 from sklearn.neighbors import KDTree
 from ttex.config import Config, ConfigurableObject
+
+from jaix.env.utils.archive.binning_strategy import BinningStrategy
 
 
 class RVBinningStrategyConfig(Config):
