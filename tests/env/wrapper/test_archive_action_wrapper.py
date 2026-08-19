b657728ad97d1519b3b767ddbce564168bba2bef
Vanessa <3634850+CIGbalance@users.noreply.github.com>
2026-08-19 13:28:50 +0200

    adding missing test for archive_action_wrapper (#173)
    * add forgotten tests
    
    * type fixes to work with different envs
    
    * more robustness
    
    * fixing for other test as well

---
+++ tests/env/wrapper/test_archive_action_wrapper.py
@@ -0,0 +1,69 @@
+from jaix.env.wrapper.archive_action_wrapper import (
+    ArchiveActionWrapper,
+    ArchiveActionWrapperConfig,
+)
+from ..utils.archive.test_archive import DummyArchive
+from jaix.env.utils.archive.action_space import (
+    UniformCrossoverActionSpace,
+    UniformCrossoverActionSpaceConfig,
+)
+from jaix.env.wrapper.archive_wrapper import ArchiveWrapperConfig
+import gymnasium as gym
+
+
+def create_dummy_archive_action_wrapper(
+    max_size: int = 10, num_samples: int = 5
+) -> ArchiveActionWrapper:
+
+    # Create a dummy archive with samples
+    archive_config = ArchiveWrapperConfig(
+        archive_class=DummyArchive,
+        archive_config=None,
+        replace_reward=True,
+        passthrough=True,
+    )
+
+    action_space_config = UniformCrossoverActionSpaceConfig(num_parents=1)
+
+    wrapper_config = ArchiveActionWrapperConfig(
+        archive_wrapper_config=archive_config,
+        action_space_class=UniformCrossoverActionSpace,
+        action_space_config=action_space_config,
+    )
+    env = gym.make("Taxi-v4")  # Dummy environment for testing purposes
+
+    # Create the ArchiveActionWrapper
+    wrapper = ArchiveActionWrapper(
+        wrapper_config, env=env, max_size=max_size
+    )  # env is None for testing purposes
+
+    # Add samples to the archive
+    for i in range(num_samples):
+        sample = env.action_space.sample()  # Sample from the environment's action space
+        wrapper.archive.add(sample=sample, fitness=float(i))
+
+    return wrapper
+
+
+def test_step():
+    wrapped_env = create_dummy_archive_action_wrapper(max_size=10, num_samples=12)
+
+    obs, info = wrapped_env.reset()
+    n_points_old = wrapped_env.archive.get_archive_stats()["num_points"]
+    action = wrapped_env.action_space.sample()
+    expected_offspring = wrapped_env.archive.get(action[0])[0]
+    obs, reward, term, trunc, info = wrapped_env.step(action)
+    n_points_new = wrapped_env.archive.get_archive_stats()["num_points"]
+    assert (
+        n_points_new == n_points_old + 1
+    ), "Number of points in the archive should have increased by 1"
+    assert (
+        info["archive_action"] == expected_offspring
+    ), "The offspring in the info should match the expected offspring"
+    added_sample, added_fitness = wrapped_env.archive.get(9)
+    assert (
+        added_sample == expected_offspring
+    ), "The added sample should match the expected offspring"
+    assert (
+        added_fitness == reward
+    ), "The added fitness should match the reward returned by the step function"
